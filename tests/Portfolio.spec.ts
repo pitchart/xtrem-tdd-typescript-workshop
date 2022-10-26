af36aaededd76fbe64858f1a4695e4a87b72a499
Alexandre MOEVI <alexandre.moevi.partner@decathlon.com>
2022-10-26 11:29:23 +0200

    test: add content in Portofolio

--- tests/Portfolio.spec.ts
+++ tests/Portfolio.spec.ts
@@ -1,5 +1,6 @@
 import { Currency } from '../src/Currency'
 import { Bank } from '../src/Bank'
+import { number } from 'fp-ts'
 
 describe('Portfolio', function () {
   it('should 5 USD + 10 EUR = 17 USD', () => {
@@ -9,13 +10,29 @@ describe('Portfolio', function () {
     portfolio.add(10, Currency.EUR)
     expect(portfolio.evaluate(bank, Currency.USD)).toBe(17)
   })
+
+  it('should 10 USD + 10 EUR = 22 USD', () => {
+    const bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    const portfolio = new Portfolio()
+    portfolio.add(10, Currency.USD)
+    portfolio.add(10, Currency.EUR)
+    expect(portfolio.evaluate(bank, Currency.USD)).toBe(22)
+  })
 })
 
 class Portfolio {
+  content: number
+
+  
+  constructor() {
+    this.content = 0;
+  }
+
   evaluate (bank: Bank, currency: Currency): number {
     return 17
   }
 
   add (arg0: number, currency: Currency): void {
+    this.content = this.content + number;
   }
 }
