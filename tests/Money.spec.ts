9032ccfcf1c52241c225f5920da7d769c012129e
Erik Clairiot <47328595+erikclairiot@users.noreply.github.com>
2022-10-26 08:36:34 -0400

    test: money

--- tests/Money.spec.ts
+++ tests/Money.spec.ts
@@ -1,16 +1,24 @@
 import { Currency } from '../src/Currency'
 
 describe('Money', function () {
+
+  
   it('should add money in the same currency', () => {
     const money = new Money(50, Currency.EUR)
     const result = money.add(new Money(10, Currency.EUR))
-    expect(result).toBe(new Money(60, Currency.EUR))
+    expect(result).toEqual(new Money(60, Currency.EUR))
+  })
+
+  it('should add another money in the same currency', () => {
+    const money = new Money(100, Currency.EUR)
+    const result = money.add(new Money(10, Currency.EUR))
+    expect(result).toEqual(new Money(110, Currency.EUR))
   })
 })
 
 class Money {
-  add (arg0: Money) {
-    throw new Error('Method not implemented.')
+  add (anotherMoney: Money): Money {
+    return new Money(anotherMoney.amount + this.amount, this.currency)
   }
 
   private readonly amount: number
