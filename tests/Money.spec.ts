856c41d1e776591ec6b9950ecfcd1de7c1bc33bd
alexandrerousselledecath <alexandre.rousselle@decathlon.com>
2022-10-26 14:55:47 +0200

    test: money error when currencies are not equal

--- tests/Money.spec.ts
+++ tests/Money.spec.ts
@@ -1,21 +1,34 @@
 import { Currency } from '../src/Currency'
 
 describe('Money', function () {
-
   describe.each([
-    {a: 50, b: 10, expected: 60},
-    {a: 100, b: 10, expected: 110}
-  ])('Money sum', ({a, b, expected}) => {
-    it(`should add money in the same currency`, () => {
+    { a: 50, b: 10, expected: 60 },
+    { a: 100, b: 10, expected: 110 }
+  ])('Money sum', ({ a, b, expected }) => {
+    it('should add money in the same currency', () => {
       const money = new Money(a, Currency.EUR)
       const result = money.add(new Money(b, Currency.EUR))
       expect(result).toEqual(new Money(expected, Currency.EUR))
     })
-  });
+
+    it('should', () => {
+      const money = new Money(a, Currency.EUR)
+      expect(() => money.add(new Money(b, Currency.USD))).toThrow(MoneyError).toThrow('unable to add EUR and USD')
+    })
+  })
 })
+
+class MoneyError extends Error {
+  constructor () {
+    super('')
+  }
+}
 
 class Money {
   add (anotherMoney: Money): Money {
+    if (this.currency !== anotherMoney.currency) {
+      throw new MoneyError()
+    }
     return new Money(anotherMoney.amount + this.amount, this.currency)
   }
 
