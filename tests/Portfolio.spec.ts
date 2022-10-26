83212a8cfa472a4204e44832ca2c97b2cd805569
Stéphane Saunier <stephane.saunier@decathlon.com>
2022-10-26 11:06:22 +0200

    test: update tu

--- tests/Portfolio.spec.ts
+++ tests/Portfolio.spec.ts
@@ -1,16 +1,18 @@
 import { Currency } from '../src/Currency'
+import { Bank } from '../src/Bank'
 
 describe('Portfolio', function () {
   it('should 5 USD + 10 EUR = 17 USD', () => {
+    const bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
     const portfolio = new Portfolio()
     portfolio.add(5, Currency.USD)
     portfolio.add(10, Currency.EUR)
-    portfolio.evaluate(Currency.USD).toBe(17)
+    expect(portfolio.evaluate(bank, Currency.USD)).toBe(17)
   })
 })
 
 class Portfolio {
-  evaluate (currency: Currency) {
+  evaluate(bank: Bank, currency: Currency): number {
     throw new Error('Method not implemented.')
   }
 
