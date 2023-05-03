8c6c26b9eb5f65282ed561d0fb306d5010c1acf0
Melanie ANGLARD <melanie.anglard@decathlon.com>
2023-05-03 17:09:04 +0200

    feat: no primitive

--- tests/MoneyCalculator.spec.ts
+++ tests/MoneyCalculator.spec.ts
@@ -1,9 +1,25 @@
 import { Currency } from '../src/Currency'
 import { MoneyCalculator } from '../src/MoneyCalculator'
 
+class Money {
+  constructor(private readonly amount: number, private readonly currency: Currency) {
+  }
+  add(money: Money) {
+    return new Money(this.amount + money.amount, money.currency);
+  }
+  
+}
+
 describe('Money', function () {
   test('5 USD + 10 USD = 15 USD', () => {
     expect(MoneyCalculator.Add(5, Currency.USD, 10)).toEqual(15)
+  })
+  
+  test('5 USD + 10 USD = 15 USD', () => {
+    const money = new Money(5, Currency.USD);
+    const result = money.add(new Money(10, Currency.USD));
+
+    expect(result).toEqual(new Money(15, Currency.USD));
   })
 
   test('10 EUR x 2 = 20 EUR', () => {
