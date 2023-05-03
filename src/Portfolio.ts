5e9a96cddf6bb9c7487d838768d1185e462d9de5
yhafidi <youssef.hafidi.partner@decathlon.com>
2023-05-03 16:12:37 +0200

    feat(test): add evaluate

--- src/Portfolio.ts
+++ src/Portfolio.ts
@@ -1,13 +1,19 @@
 import { Currency } from './Currency'
+import {Bank} from "./Bank";
 
 export class Portfolio {
-    private amount : number = 0
+    private amounts :  [number, Currency][] = []
 
-    add(amount: number, EUR: Currency) {
-        this.amount += amount
+    add(amount: number, currency: Currency) {
+        this.amounts.push([amount, currency])
     }
 
-    evaluate(EUR: Currency): [number, Currency] {
-        return [this.amount, Currency.EUR]
+    evaluate(currency: Currency, bank: Bank): [number, Currency] {
+        const amount = this.amounts.map((amount:[number, Currency]):[number, Currency] => {
+            return [bank.Convert(amount[0],amount[1], currency), currency]
+        }).reduce((acc: number, currentValue:[number, Currency]): number => {
+            return acc + currentValue[0]
+        },0)
+        return [amount, currency]
     }
 }
--- tests/Portfolio.spec.ts
+++ tests/Portfolio.spec.ts
@@ -1,18 +1,53 @@
 import { Currency } from '../src/Currency'
 import { Portfolio } from '../src/Portfolio'
+import {Bank} from "../src/Bank";
 
 describe('Portfolio', function () {
     describe('Contains list of amount in different currencies', function () {
+        let portfolio;
+        let bank: Bank
+
+        beforeEach(()=>{
+            portfolio = new Portfolio();
+            bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+        })
+
         test('empty portfolio evaluate to 0 EUR', () => {
-            const portfolio = new Portfolio();
-            expect(portfolio.evaluate(Currency.EUR)).toEqual([0, Currency.EUR])
+            expect(portfolio.evaluate(Currency.EUR, bank)).toEqual([0, Currency.EUR])
         })
 
         test('portfolio with 5 EUR evaluate to 5 EUR', () => {
-            const portfolio = new Portfolio();
             portfolio.add(5, Currency.EUR);
-            expect(portfolio.evaluate(Currency.EUR)).toEqual([5, Currency.EUR])
+            expect(portfolio.evaluate(Currency.EUR, bank)).toEqual([5, Currency.EUR])
         })
+
+        test('portfolio with 5 EUR and 5 EUR evaluate to 10 EUR', () => {
+            portfolio.add(5, Currency.EUR);
+            portfolio.add(5, Currency.EUR);
+            expect(portfolio.evaluate(Currency.EUR, bank)).toEqual([10, Currency.EUR])
+        })
+
+
+
     })
- 
+  describe( 'we can evaluate the total amount of portfolio in a given currency', ()=>{
+      let portfolio: Portfolio;
+      let bank: Bank
+
+
+      beforeEach(()=>{
+          portfolio = new Portfolio();
+          bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+          bank.AddExchangeRate(Currency.EUR, Currency.KRW, 1344)
+          bank.AddExchangeRate(Currency.USD, Currency.KRW, 1100)
+
+
+      })
+      test('portfolio with 5 USD and 10 EUR evaluate to 17 USD', () => {
+          portfolio.add(5, Currency.USD);
+          portfolio.add(10, Currency.EUR);
+
+          expect(portfolio.evaluate(Currency.USD, bank)).toEqual([17, Currency.USD])
+      })
+  })
 })
