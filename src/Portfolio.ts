393ed992efb0436d05219a38ebf5caea3b975438
Julien VITTE <julien.vitte@insidegroup.fr>
2023-06-23 17:07:20 +0200

    refacto: portfolio evaluate

--- src/Portfolio.ts
+++ src/Portfolio.ts
@@ -1,13 +1,12 @@
-import { Currency } from "./Currency";
-import { Bank } from "./Bank";
-import {Money} from "./Money";
-import { Throwable } from "funfix-core";
-import { Either, Try } from "funfix-core";
+import { Currency } from './Currency'
+import { Bank } from './Bank'
+import { Money } from './Money'
+import { Throwable, Either, Try } from 'funfix-core'
 
 export class Portfolio {
   private readonly moneys: Money[] = []
 
-  add(money: Money): void {
+  add (money: Money): void {
     this.moneys.push(money)
   }
 
@@ -17,10 +16,15 @@ export class Portfolio {
         return bank.Convert(money, currency)
       })
       .reduce((acc: Either<Throwable, Money>, currentValue: Either<Throwable, Money>): Either<Throwable, Money> => {
-        if (currentValue.isLeft()) return currentValue;
-        // if (acc.isLeft()) return acc;
-
-        return acc.flatMap((money: Money) => Try.of(() => money.add(currentValue.get())).toEither())
+        return this.addMoneys(acc, currentValue)
       }, Either.right(new Money(0, currency)))
+  }
+
+  private addMoneys (acc: Either<Throwable, Money>, currentValue: Either<Throwable, Money>): Either<Throwable, Money> {
+    return acc.flatMap((money: Money) =>
+      currentValue.flatMap(currentMoney =>
+        Try.of(() => currentMoney.add(money)).toEither()
+      )
+    )
   }
 }
